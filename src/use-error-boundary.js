0c67704e204e19c7fab029d3e79d4c8784551620
Joschua Schneider <joschua-schneider@web.de>
2019-05-22 09:40:02 +0200

    renamed variables and comments to new wrapper utility

--- src/use-error-boundary.js
+++ src/use-error-boundary.js
@@ -1,14 +1,14 @@
 import { useRef, useReducer } from "react"
 
-import { createErrorBoundaryClass } from "./utils/create-error-boundary"
+import { createWrappedErrorBoundary } from "./utils/create-wrapped-error-boundary"
 
 /**
  * useErrorBoundary
  * React hook to use an ErrorBoundary in your component and keep track of the
  * error state of that boundary.
  *
- * Uses a class component to create the error Boundary, but uses hooks to keep the state
- * in your component.
+ * Uses a wrapped class component to create the error Boundary, but uses hooks to keep the state
+ * in your function component.
  */
 export function useErrorBoundary() {
   // Reducer handling the error state
@@ -36,21 +36,21 @@ export function useErrorBoundary() {
       errorInfo: null
     }
   )
-  // Create ref for ErrorBoundary class
-  const errorBoundaryClassRef = useRef(null)
+  // Create ref for wrapped ErrorBoundary class
+  const errorBoundaryWrapperRef = useRef(null)
 
-  // Get the current ref value or initialize it with a new ErrorBoundary class
-  function getErrorBoundaryClass() {
+  // Get the current ref value or initialize it with a new wrapped ErrorBoundary
+  function getWrappedErrorBoundary() {
     // Get current ref value
-    let errorBoundaryClass = errorBoundaryClassRef.current
+    let errorBoundaryWrapper = errorBoundaryWrapperRef.current
 
-    // Return the current class when already initialized
-    if (errorBoundaryClass !== null) {
-      return errorBoundaryClass
+    // Return the component when already initialized
+    if (errorBoundaryWrapper !== null) {
+      return errorBoundaryWrapper
     }
 
-    // Create new ErrorBoundary class with onDidCatch callback
-    errorBoundaryClass = createErrorBoundaryClass({
+    // Create new wrapped ErrorBoundary class with onDidCatch callback
+    errorBoundaryWrapper = createWrappedErrorBoundary({
       // onDidCatch callback
       onDidCatch(err, errInfo) {
         // Dispatch action in case of an error
@@ -62,16 +62,16 @@ export function useErrorBoundary() {
       }
     })
 
-    // Update the ref with new class
-    errorBoundaryClassRef.current = errorBoundaryClass
+    // Update the ref with new component
+    errorBoundaryWrapperRef.current = errorBoundaryWrapper
 
-    // Return the newly created class
-    return errorBoundaryClass
+    // Return the newly created component
+    return errorBoundaryWrapper
   }
 
-  // Return the ErrorBoundary class to wrap your components in and the error state
+  // Return the wrapped ErrorBoundary class to wrap your components in plus the error state
   return {
-    ErrorBoundary: getErrorBoundaryClass(),
+    ErrorBoundary: getWrappedErrorBoundary(),
     didCatch,
     error,
     errorInfo
